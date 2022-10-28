33e3594ce3c4a0dca67bd86815544ec2abb1ef1f
jacobboney <jacobaboney@gmail.com>
2022-10-28 01:12:11 -0400

    Remaining frameworks added to react app
    Bootstrap:
    -Allows for easy construction of UI with prebuilt/modular components
    -Temporary Navbar was added to ensure compatability
    
    Bootswatch:
    -Promotes consistent styling of components
    -"Journal" style was selected to ensure compatability (can be changed/modified later)
    
    Plotly:
    -Added for visualization of data
    -Simple example chart was added to homepage to ensure compatability and view features
    
    **Again, "npm install" must be run in /client and /server for the changes to take effect**
    (I'll try and see if I can work this into a single command to run in the root directory similarly to "npm run dev" to make it easier when changes are made)

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 import React from "react";
+//import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import Plot from 'react-plotly.js';
 
 function App() {
 
-    const [data, setData] = React.useState(null);
-
-    React.useEffect(() => {
-        fetch("/api")
-            .then((res) => res.json())
-            .then((data) => setData(data.message));
-    }, []);
-
     return (
-        <div className="App">
-            <header className="App-header">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    {!data ? "Loading..." : data}
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Learn React
-                </a>
-            </header>
-        </div>
+        <>
+            <Navbar bg="primary" variant="dark" expand="lg">
+                <div className="container">
+                    <Navbar.Brand href="#home">Covid Analyzer</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#home">Home</Nav.Link>
+                            <Nav.Link href="#query1">Query01</Nav.Link>
+                            <Nav.Link href="#query2">Query02</Nav.Link>
+                            <Nav.Link href="#query3">Query03</Nav.Link>
+                            <Nav.Link href="#query4">Query04</Nav.Link>
+                            <Nav.Link href="#query5">Query05</Nav.Link>
+                            <Nav.Link href="#sources">Sources</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </div>
+            </Navbar>
+            <div className="d-flex justify-content-center">
+                <Plot
+                    data={[
+                        {
+                            x: [1, 2, 3],
+                            y: [2, 6, 3],
+                            type: 'scatter',
+                            mode: 'lines+markers',
+                            marker: {color: 'red'},
+                        },
+                        {type: 'bar', x: [1, 2, 3], y: [2, 5, 3]},
+                    ]}
+                    layout={ {width: 320, height: 240, title: 'A Fancy Plot'} }
+                />
+            </div>
+
+        </>
     );
 }
 
