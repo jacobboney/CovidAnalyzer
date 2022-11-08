b79ec84a6bb1ad12764b9a6aa0d0d3e086f7ee1d
jacobboney <jacobaboney@gmail.com>
2022-11-07 23:57:23 -0500

    Skeletons of pages
    Added necessary pages, and icon for github logo

---
+++ client/src/pages/Vaccinations.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Vaccinations(){
+    return(
+        <div>
+            <h1>Vaccinations Test</h1>
+        </div>
+    )
+
+};
+
+export default Vaccinations;
