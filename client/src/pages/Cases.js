6ee19545983e1bebf6c851ed0edd6d8f8cb3378f
Gabriell Yap <g.yap@ufl.edu>
2022-11-08 13:45:09 -0500

    added content and CSS to cases page

--- client/src/pages/Cases.js
+++ client/src/pages/Cases.js
@@ -4,6 +4,19 @@ function Cases(){
     return(
         <div>
             <h1>Cases Page Test</h1>
+            <div className="pageContent">
+                <div className="parametersContainer">
+                    <p className="parameterItem">% of cases over time</p>
+                    <p className="parameterItem">% of cases leading to hosp</p>
+                    <p className="parameterItem">ratio of deaths to cases over time</p>
+                    <p className="parameterItem">leading causes of deaths(per age) over time</p>
+                    <p className="parameterItem"># cases based on the season</p>
+                </div>
+
+                <div className="visContainer">
+
+                </div>
+            </div>
         </div>
     )
 
