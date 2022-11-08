773dfa84698b7c4f1a85c7b3b6b71e8571c50a53
Gabriell Yap <g.yap@ufl.edu>
2022-11-08 14:03:29 -0500

    added state to cases

--- client/src/pages/Cases.js
+++ client/src/pages/Cases.js
@@ -1,20 +1,47 @@
-import React from "react";
+import React, {useState
+} from "react";
 
 function Cases(){
+    const [selectedParam, setSelectedParam] = useState("")
+    function handleClick(e){
+        setSelectedParam(e.target.id)
+    }
+  
+    function showVis(){
+        switch(selectedParam){
+            case "casesOverTime":
+                return "chart Cases over Time"
+
+            case "hosp":
+                return "%cases hosp"
+
+            case "deathsOT":
+                return "deaths"
+
+            case "causes":
+                return "causes"
+            
+            case "season":
+                return "season"
+
+            default:
+                return
+        }
+    }
     return(
         <div>
             <h1>Cases Page Test</h1>
             <div className="pageContent">
                 <div className="parametersContainer">
-                    <p className="parameterItem">% of cases over time</p>
-                    <p className="parameterItem">% of cases leading to hosp</p>
-                    <p className="parameterItem">ratio of deaths to cases over time</p>
-                    <p className="parameterItem">leading causes of deaths(per age) over time</p>
-                    <p className="parameterItem"># cases based on the season</p>
+                    <p className="parameterItem" id="casesOverTime" onClick={handleClick}>% of cases over time</p>
+                    <p className="parameterItem" id="hosp" onClick={handleClick}>% of cases leading to hosp</p>
+                    <p className="parameterItem" id="deathsOT" onClick={handleClick}>ratio of deaths to cases over time</p>
+                    <p className="parameterItem" id="causes" onClick={handleClick}>leading causes of deaths(per age) over time</p>
+                    <p className="parameterItem" id="season" onClick={handleClick}># cases based on the season</p>
                 </div>
 
                 <div className="visContainer">
-
+                    {showVis()}
                 </div>
             </div>
         </div>
