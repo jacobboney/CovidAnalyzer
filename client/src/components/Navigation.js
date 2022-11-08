bb0f43fa484277774b1387d2a0f4a698e5ca6380
jacobboney <jacobaboney@gmail.com>
2022-11-07 23:56:40 -0500

    Navbar / Footer
    Added navigation bar and footer to project as components

---
+++ client/src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React from "react";
+import {Navbar, Nav} from "react-bootstrap";
+import {LinkContainer} from "react-router-bootstrap"
+
+
+
+const Navigation = () =>{
+        return(
+                <Navbar bg="primary" variant="dark" expand="lg">
+                    <div className="container">
+                        <LinkContainer to="/">
+                        <Navbar.Brand href="/">Covid Analyzer</Navbar.Brand>
+                        </LinkContainer>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                            <Nav className="me-auto">
+                                <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                                <LinkContainer to="/cases"><Nav.Link>Cases</Nav.Link></LinkContainer>
+                                <LinkContainer to="/vaccinations"><Nav.Link>Vaccinations</Nav.Link></LinkContainer>
+                                <LinkContainer to="/global-distributions"><Nav.Link>Global Distributions</Nav.Link></LinkContainer>
+                                <LinkContainer to="/data-sources"><Nav.Link>Data Sources</Nav.Link></LinkContainer>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </div>
+                </Navbar>
+        )
+}
+
+export default Navigation;
