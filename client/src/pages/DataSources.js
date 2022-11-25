511a912a390480ce6d082f5e634c60d8d15e223d
Gabriell Yap <g.yap@ufl.edu>
2022-11-25 13:34:02 -0500

    added data sources; finished layout for other pages

--- client/src/pages/DataSources.js
+++ client/src/pages/DataSources.js
@@ -1,9 +1,30 @@
 import React from "react";
 
-function DataSources(){
-    return(
+function DataSources() {
+    return (
         <div>
-            <h1>Data Sources Test</h1>
+            <h1 className="text-center my-4">Data Sources</h1>
+            <ul>
+                <li className="my-3"> 
+                    COVID Vaccinations: <a href="https://ourworldindata.org/covid-vaccinations" target="_blank" rel="noreferrer">OurWorld in Data</a> 
+                </li>
+
+                <li className="my-3"> 
+                    COVID Vaccinations in US: <a href="https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-Jurisdi/unsk-b7fc" target="_blank" rel="noreferrer">CDC</a> 
+                </li>
+
+                <li className="my-3">
+                    Provisional COVID-19 Deaths by Sex and Age: <a href="https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-by-Sex-and-Age/9bhg-hcku" target="_blank" rel="noreferrer">CDC</a>
+                </li>
+
+                <li className="my-3">
+                    Conditions Contributing to COVID-19 Deaths, by State and Age, Provisional 2020-2022: <a href="https://data.cdc.gov/NCHS/Conditions-Contributing-to-COVID-19-Deaths-by-Stat/hk9y-quqm" target="_blank" rel="noreferrer">CDC</a>
+                </li>
+
+                <li className="my-3"> 
+                    Provisional COVID-19 Deaths: Distribution of Deaths by Race and Hispanic Origin: <a href="https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-Distribution-of-Deaths/pj7m-y5uh" target="_blank" rel="noreferrer">CDC</a> 
+                </li>
+            </ul>
         </div>
     )
 
--- client/src/pages/GlobalDistribution.js
+++ client/src/pages/GlobalDistribution.js
@@ -1,9 +1,51 @@
-import React from "react";
+import React, {useState
+} from "react";
 
 function GlobalDistribution(){
+
+    const [selectedParam, setSelectedParam] = useState("")
+    function handleClick(e){
+        setSelectedParam(e.target.id)
+    }
+
+    function showVis(){
+        switch(selectedParam){
+            case "casesOverTime":
+                return "chart Cases over Time"
+
+            case "hosp":
+                return "%cases hosp"
+
+            case "deathsOT":
+                return "deaths"
+
+            case "causes":
+                return "causes"
+            
+            case "season":
+                return "season"
+
+            default:
+                return
+        }
+    }
+
     return(
-        <div>
-            <h1>Global Distribution Test</h1>
+        <div className="pageContainer">
+            <h1 className="text-center my-4">Global Distribution</h1>
+            <div className="pageContent">
+                <div className="parametersContainer">
+                    <p className="parameterItem" id="casesOverTime" onClick={handleClick}>% of cases over time</p>
+                    <p className="parameterItem" id="hosp" onClick={handleClick}>% of cases leading to hosp</p>
+                    <p className="parameterItem" id="deathsOT" onClick={handleClick}>ratio of deaths to cases over time</p>
+                    <p className="parameterItem" id="causes" onClick={handleClick}>leading causes of deaths(per age) over time</p>
+                    <p className="parameterItem" id="season" onClick={handleClick}># cases based on the season</p>
+                </div>
+
+                <div className="visContainer">
+                    {showVis()}
+                </div>
+            </div>
         </div>
     )
 
