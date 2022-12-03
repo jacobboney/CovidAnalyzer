14312a6b75ce6f48965bc75757a2fcbd95828fdd
Jacob Boney <jacobaboney@gmail.com>
2022-12-03 14:04:56 -0500

    API update
    Update to the API to support database calls for each chart

--- server/index.js
+++ server/index.js
@@ -27,6 +27,118 @@ app.get("/test", async (req, res) => {
     res.json(data);
 })
 
+////APIs for Cases Page
+//Cases Over Time
+app.get("/casesCOT", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Cases Leading to Hosp
+app.get("/casesCLH", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Ratio of Deaths to Cases
+app.get("/casesRDTC", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Leading Causes of Death
+app.get("/casesCOD", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Cases Based on Season
+app.get("/casesBOS", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+
+////APIs for Vaccinations Page
+//Cases Over Time
+app.get("/vaccCOT", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Cases Leading to Hosp
+app.get("/vaccCLH", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Ratio of Deaths to Cases
+app.get("/vaccRDTC", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Leading Causes of Death
+app.get("/vaccCOD", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Cases Based on Season
+app.get("/vaccBOS", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+
+////APIs for Global Distributions Page
+//Cases Over Time
+app.get("/globalCOT", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Cases Leading to Hosp
+app.get("/globalCLH", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Ratio of Deaths to Cases
+app.get("/globalRDTC", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Leading Causes of Death
+app.get("/globalCOD", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+//Cases Based on Season
+app.get("/globalBOS", async (req, res) => {
+    let sql = `select * from CONTINENT`;
+    let data = await query(sql);
+    console.log(data);
+    res.json(data);
+})
+
+
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
