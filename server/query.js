3486d345b783e95db1b8be471dc2cfdbdda1a9ef
Jacob Boney <jacobaboney@gmail.com>
2022-12-04 21:54:39 -0500

    Cleaned up query.js

--- server/query.js
+++ server/query.js
@@ -1,7 +1,13 @@
 import oracledb from 'oracledb';
 import config from "./config.js";
-
-//let st = `select * from AIRPORT`; //input string for testing
+/*
+let st = `select * from(
+select "Monthly_Cases", to_date(to_char("Year") || to_char("Month") || '01', 'yyyy/mm/dd') as "Date" from(
+select sum(cases) as "Monthly_Cases", to_char(true_date, 'MM') as "Month", to_char(true_date, 'YYYY') as "Year" from casedata
+where iso_code = 'ABW'
+group by to_char(true_date, 'MM'), to_char(true_date, 'YYYY')
+order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))
+`; //input string for testing */
 
 async function query(statement){
     let connection;
