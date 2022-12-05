8b8758ab665767b4652aaa6c8936f0c1500a3d2f
Jacob Boney <jacobaboney@gmail.com>
2022-12-05 11:08:45 -0500

    Styling updates

--- client/src/pages/DataSources.js
+++ client/src/pages/DataSources.js
@@ -1,30 +1,36 @@
 import React from "react";
+import covidImage2 from "../components/covidImage2.png";
 
 function DataSources() {
     return (
-        <div>
-            <h1 className="text-center my-4">Data Sources</h1>
-            <ul>
-                <li className="my-3"> 
-                    COVID Vaccinations: <a href="https://ourworldindata.org/covid-vaccinations" target="_blank" rel="noreferrer">OurWorld in Data</a> 
+        <div style={{backgroundImage:`url(${covidImage2})`, backgroundSize:"cover", width:'100vw', height:'100vh', position:"fixed"}}>
+            <div className = "d-flex justify-content-center">
+                <h1 className="m-5 p-4 bg-primary text-light rounded">Data Sources</h1>
+            </div>
+
+            <div className="d-flex justify-content-center ">
+            <ul className="bg-primary text-light rounded px-5 py-2">
+                <li className="my-3 ">
+                    COVID Vaccinations: <a href="https://ourworldindata.org/covid-vaccinations" target="_blank" rel="noreferrer" className="link-info">OurWorld in Data</a>
                 </li>
 
                 <li className="my-3"> 
-                    COVID Vaccinations in US: <a href="https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-Jurisdi/unsk-b7fc" target="_blank" rel="noreferrer">CDC</a> 
+                    COVID Vaccinations in US: <a href="https://data.cdc.gov/Vaccinations/COVID-19-Vaccinations-in-the-United-States-Jurisdi/unsk-b7fc" target="_blank" rel="noreferrer" className="link-info">CDC</a>
                 </li>
 
                 <li className="my-3">
-                    Provisional COVID-19 Deaths by Sex and Age: <a href="https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-by-Sex-and-Age/9bhg-hcku" target="_blank" rel="noreferrer">CDC</a>
+                    Provisional COVID-19 Deaths by Sex and Age: <a href="https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-by-Sex-and-Age/9bhg-hcku" target="_blank" rel="noreferrer" className="link-info">CDC</a>
                 </li>
 
                 <li className="my-3">
-                    Conditions Contributing to COVID-19 Deaths, by State and Age, Provisional 2020-2022: <a href="https://data.cdc.gov/NCHS/Conditions-Contributing-to-COVID-19-Deaths-by-Stat/hk9y-quqm" target="_blank" rel="noreferrer">CDC</a>
+                    Conditions Contributing to COVID-19 Deaths, by State and Age, Provisional 2020-2022: <a href="https://data.cdc.gov/NCHS/Conditions-Contributing-to-COVID-19-Deaths-by-Stat/hk9y-quqm" target="_blank" rel="noreferrer" className="link-info">CDC</a>
                 </li>
 
                 <li className="my-3"> 
-                    Provisional COVID-19 Deaths: Distribution of Deaths by Race and Hispanic Origin: <a href="https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-Distribution-of-Deaths/pj7m-y5uh" target="_blank" rel="noreferrer">CDC</a> 
+                    Provisional COVID-19 Deaths: Distribution of Deaths by Race and Hispanic Origin: <a href="https://data.cdc.gov/NCHS/Provisional-COVID-19-Deaths-Distribution-of-Deaths/pj7m-y5uh" target="_blank" rel="noreferrer" className="link-info">CDC</a>
                 </li>
             </ul>
+            </div>
         </div>
     )
 
--- server/index.js
+++ server/index.js
@@ -23,7 +23,11 @@ app.get("/test", async (req, res) => {
     res.json(data);
 })
 
-////APIs for Cases Page
+
+
+
+////APIs for Cases Page////
+
 //Monthly Cases Over Time
 app.get("/casesMonthly", async (req, res) => {
     let sql = `select * from(
@@ -38,23 +42,21 @@ order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))
     res.json(data);
 })
 
+//Percentage of Cases to Deaths
 app.get("/casesToDeathPercent", async (req, res) => {
     let sql = `select ("Monthly_Deaths" / "Monthly_Cases") as "Percentage", t1."Date", t1."Code" from(
-
 (select * from(
 select "Monthly_Deaths", to_date(to_char("Year") || to_char("Month") || '01', 'yyyy/mm/dd') as "Date", "Code" from(
 select sum(deaths) as "Monthly_Deaths", to_char(true_date, 'MM') as "Month", to_char(true_date, 'YYYY') as "Year", casedata.iso_code as "Code" from casedata
 where iso_code = 'USA'
 group by to_char(true_date, 'MM'), to_char(true_date, 'YYYY'), casedata.iso_code
 order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))) t1
-
 inner join(select * from(
 select "Monthly_Cases", to_date(to_char("Year") || to_char("Month") || '01', 'yyyy/mm/dd') as "Date", "Code" from(
 select sum(cases) as "Monthly_Cases", to_char(true_date, 'MM') as "Month", to_char(true_date, 'YYYY') as "Year", casedata.iso_code as "Code" from casedata
 where iso_code = 'USA'
 group by to_char(true_date, 'MM'), to_char(true_date, 'YYYY'), casedata.iso_code
 order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))) t2
-
 on t1."Date"=t2."Date")
 `;
     let data = await query(sql);
@@ -62,6 +64,8 @@ on t1."Date"=t2."Date")
     res.json(data);
 })
 
+
+//Cases per Capita vs HDI
 app.get("/casesVsHDI", async (req, res) => {
     let sql = `select "Country", round(("TotalCases" / "Population"), 5) as "PerCapita", "HDI" from(
 (select casedata.iso_code as "Country", sum(casedata.cases) as "TotalCases", population.population as "Population" from casedata
@@ -75,6 +79,8 @@ where "Country"!='NAM'
     res.json(data);
 })
 
+
+//Total Cases Based on the Season
 app.get("/casesBOS", async (req, res) => {
     let sql = `select sum(cases) as "TotalCases", season, iso_code as "Country" from casedata
 where iso_code='USA'
@@ -87,7 +93,9 @@ group by season, iso_code
 
 
 
+////APIs for Vaccinations Page////
 
+//Monthly Vaccinations
 app.get("/vaccMonthly", async (req, res) => {
     let sql = `select * from(
 select "Monthly_Vacc", to_date(to_char("Year") || to_char("Month") || '01', 'yyyy/mm/dd') as "Date", "Code" from(
@@ -101,9 +109,7 @@ order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))
     res.json(data);
 })
 
-
-
-
+//Vaccinations per Capita Globally
 app.get("/vaccPerCapita", async (req, res) => {
     let sql = `select Country, round((totalvax / population), 5) as perCapita from(
 select daily_vax.iso_code as Country, sum(daily_vax.daily_vaccinations) as TotalVax, population.population as Population from daily_vax
@@ -117,25 +123,21 @@ order by perCapita asc
     res.json(data);
 })
 
-
-
+//Vaccinations vs Cases
 app.get("/vaccVsCases", async (req, res) => {
     let sql = `select "Monthly_Cases" as "Cases", "Monthly_Vacc" as "Vacc", t1."Date", t1."Code" from(
-
 (select * from(
 select "Monthly_Cases", to_date(to_char("Year") || to_char("Month") || '01', 'yyyy/mm/dd') as "Date", "Code" from(
 select sum(cases) as "Monthly_Cases", to_char(true_date, 'MM') as "Month", to_char(true_date, 'YYYY') as "Year", casedata.iso_code as "Code" from casedata
 where iso_code = 'USA'
 group by to_char(true_date, 'MM'), to_char(true_date, 'YYYY'), casedata.iso_code
 order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))) t1
-
 inner join (select * from(
 select "Monthly_Vacc", to_date(to_char("Year") || to_char("Month") || '01', 'yyyy/mm/dd') as "Date", "Code" from(
 select sum(daily_vaccinations) as "Monthly_Vacc", to_char(true_date, 'MM') as "Month", to_char(true_date, 'YYYY') as "Year", daily_vax.iso_code as "Code" from daily_vax
 where iso_code = 'USA'
 group by to_char(true_date, 'MM'), to_char(true_date, 'YYYY'), daily_vax.iso_code
 order by to_char(true_date, 'YYYY'), to_char(true_date, 'MM') asc))) t2
-
 on t1."Date"=t2."Date")
 `;
     let data = await query(sql);
@@ -144,7 +146,9 @@ on t1."Date"=t2."Date")
 })
 
 
+////APIs for GLobal Distribution Page////
 
+//Total Cases vs HDI vs Hand-Washing
 app.get("/globalCasesVsHDIvsHW", async (req, res) => {
     let sql = `select "TotalCases", "HDI", "HWF" from(
 (select * from(
