4b4b2c98fd58b8657b4d2906c2f6875ad52377d8
Jacob Boney <jacobaboney@gmail.com>
2022-12-04 21:54:18 -0500

    Updated config to proper database

--- server/config.js
+++ server/config.js
@@ -3,8 +3,13 @@
 //Must be connected to UFL VPN for database querys to return, however, code will still function//
 
 const config = {
+    /*
     user: "jacobboney", //This will be the user's UFL ID
     password: "HQNXApvoRLy6ZK9snAxeXd7L", //This is the user's connection string created for the UFL Oracle Database
+     */
+    user: "andrewkrawczyk", //This will be the user's UFL ID
+    password: "6RqhRdlwL51uHKnkTSTwsoa1", //This is the user's connection string created for the UFL Oracle Database
+
     connectionString: "//oracle.cise.ufl.edu:1521/orcl" //This connection string is neccessary to connect to Oracle Database ***DO NOT CHANGE***
 }
 
